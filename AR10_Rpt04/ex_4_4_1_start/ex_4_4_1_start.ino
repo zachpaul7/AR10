c3197bbf99e03a65b088837b620a320e36629f31
GodZero <60610390+zachpaul7@users.noreply.github.com>
2022-04-07 16:28:17 +0900

    Add files via upload

---
+++ AR10_Rpt04/ex_4_4_1_start/ex_4_4_1_start.ino
@@ -0,0 +1,56 @@
+/*
+ 예제 4.4.1
+ FND 제어 0~9까지 1초단위로 표시하기
+*/
+ 
+ // 0~9까지 LED 표시를 위한 상수 설정
+const byte number[10] = {
+//dot  gfedcba
+  B11000000,  //0
+  B11111001,  //1
+  B10100100,  //2
+  B10110000,  //3
+  B10011001,  //4
+  B10010010,  //5
+  B10000010,  //6
+  B11111000,  //7
+  B10000000,  //8
+  	//9
+};
+
+void setup()
+{
+  // 2~9번 핀을 a b c d e f g dot 의 순서로 사용한다
+  // 2~9번핀을 출력으로 초기화 시킨다.
+  for(int i = 2; i <= 9; ++i){
+    pinMode(i,OUTPUT);
+  };
+  	// 점은 표시하지 않는다
+    
+}
+
+void loop()
+{
+   // k값을 0~9로 변화시킨다.
+  for(int k = 0; k <= 9; ++k){
+   fndDisplay(k);  // k값을 출력한다
+   delay(1000);
+  };
+} 
+
+// LED 점등
+void fndDisplay(int displayValue){
+  // bitValue 변수를 선언한다.
+  boolean bitValue;
+  	
+  for(int i=2; i<=9; ++i){	
+    // 2~9번핀에 모두 LOW 신호를 줘서 소등시킨다
+    digitalWrite(i, LOW);
+  };
+  for(int i=0; i<=7; ++i){
+    // number 상수의 하나의 비트값을 읽는다
+    
+    // 앞서 읽은 비트값을 2~9번핀에 출력시킨다
+    
+  };
+}
---
+++ AR10_Rpt04/ex_4_4_2/ex_4_4_2.ino
@@ -0,0 +1,63 @@
+/*
+ 예제 4.4.2
+ 74595를 이용하여 FND 제어 0~9까지 
+ 1초단위로 표시하기
+*/
+ 
+ // 0~9까지 LED 표시를 위한 상수 설정
+const byte number[16] = {
+//dot  gfedcba
+  B11000000,    //0
+  B11111001,    //1
+  B10100100,    //2
+  B10110000,    //3
+  B10011001,    //4
+  B10010010,    //5
+  B10000010,    //6
+  B11111000,    //7
+  B10000000,    //8
+  B10010000,	//9
+  B10001000,  //A
+  B10000011,  //B
+  B11000110,  //C
+  B10100001,  //D
+  B10000110,  //E
+  B10001110 //F
+  
+};
+
+int ds = 2;
+int shcp = 3;
+int stcp = 4;
+
+ // 74595의 DS핀을 Arduino의 2번핀에 연결
+  // 74595의 STCP핀을 Arduino의 3번핀에 연결
+  // 74595의 SHSP핀을 Arduino의 4번핀에 연결
+
+void setup()
+{
+  // 2~9번핀을 출력으로 초기화 시킨다.
+  for(int i = 2; i <= 9; ++i){
+    pinMode(i,OUTPUT);
+  };
+  digitalWrite(9,LOW);	// 점은 표시하지 않는다
+}
+
+void loop()
+{
+   // k값을 0~9로 변화시킨다.
+  for(int k = 0; k <= 15; ++k){
+   fndDisplay74595(k);  // k값을 출력한다
+   delay(1000);         // 1초간 지연시킨다.         
+  };
+} 
+
+// LED 점등
+void fndDisplay74595(int displayValue){
+  digitalWrite(stcp,LOW);// STCP에 LOW 신호를 보내서 74595로 데이터전송을 시작한다.
+  
+  shiftOut(ds,shcp,MSBFIRST,number[displayValue]);// shiftOut 명령어로 74595에 출력을 보낸다.
+  
+  digitalWrite(stcp,HIGH);// STCP에 HIGH 신호를 보내서 74595로 데이터전송을 종료한다.
+  
+}
